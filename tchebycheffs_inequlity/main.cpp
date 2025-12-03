1c8a6f66d4a00b46bc4f4f41a0c2834ab12bfb54
joeurassa <johurassa@gmail.com>
2025-12-02 18:23:11 -0600

    Initial Commit

---
+++ tchebycheffs_inequlity/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tchebycheffs_inequlity
+//
+//  Created by Joe on 12/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
